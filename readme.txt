a6a8b5690a2120ad80c8f68ed36cea92d9498767
JoseaSosa <josea_1998@htomail.com>
2023-02-16 11:49:49 -0500

    Changes

---
+++ readme.txt
@@ -0,0 +1,43 @@
+PREGUNTAS:
+
+- ¿Cuántas clases tiene el proyecto?
+
+	Tiene 3 clases Animal, Familia y Main
+
+- ¿En cuál clase se define el programa principal
+
+	Zoologico/Main
+
+- ¿Cuántos objetos de la clase Animal se crean en la clase
+principal?
+
+	Dos objetos animal1 y animal2
+
+- ¿Cuántos y cuáles son los atributos de la clase Animal?
+
+	son 4 Nombre, genero, peso y pareja
+
+- ¿Cuáles atributos de la clase Animal no son primitivos?
+
+	El atributo pareja es de la clase animal, el resto son String o double
+
+- ¿Qué pasa si eliminamos la línea 17 de la clase Zoologico?
+
+	El metodo encontrarPareja no se ejecutaria y no se asigna ninguna pareja
+
+- ¿Cuántos métodos tiene la clase Familia?
+
+	2 Metodos
+
+- ¿Cuántos parámetros tiene el método tenerHijo() de la clase
+Familia?
+
+	Solo uno, ..(String nombre){
+
+- ¿Cuántos atributos inicializa el constructor de la clase Familia?
+
+	El constructor por default tiene 2 atributos (Animal papa, Animal mama)
+
+- ¿Qué tipo de retorno tiene el método tenerHijo()?
+
+	Ninguno, void. Pero asigna el hijo. 
--- src/main/java/org/example/Familia.java
+++ src/main/java/org/example/Familia.java
@@ -2,40 +2,61 @@ package org.example;
 
 public class Familia {
 
-    Animal papa="Dinosaurin";
-    Animal mama="Tiburosin";
-    Animal hijo;
+    private Animal papa;
+    private Animal mama;
+    private Animal hijo;
 
+    public Familia(){}
     public Familia(Animal papa, Animal mama) {
         this.papa = papa;
         this.mama = mama;
     }
+    public Familia(Animal papa, Animal mama, Animal hijo) {
+        this.papa = papa;
+        this.mama = mama;
+        this.hijo = hijo;
+    }
 
-    public  imprimirFamilia(){
+    public Animal getPapa() {return papa;}
+    public void setPapa(Animal papa) {this.papa = papa;}
+    public Animal getMama() {return mama;}
+    public void setMama(Animal mama) {this.mama = mama;}
+    public Animal getHijo() {return hijo;}
+    public void setHijo(Animal hijo) {this.hijo = hijo;}
 
-        //Este metodo muestra los nombres de los integrantes de la familia
-        System.out.println(
-                "El papá es: "+this.papa.nombre+
-                        "\nLa mamá es: "+this.mama.nombre+
-                            "\nEl hijo es: "+this.hijo.nombre
-        );
 
+    public void imprimirFamilia(){
+        if (this.hijo == null){
+            System.out.println(
+                    "El papa es: " + this.papa.getNombre() +
+                            "\nLa mama es: " + this.mama.getNombre()
+            );
+        } else {
+            System.out.println(
+                    "El papa es: " + this.papa.getNombre() +
+                            "\nLa mama es: " + this.mama.getNombre() +
+                            "\nEl hijo es: " + this.hijo.getNombre()
+            );
+        }
     }
 
     public void tenerHijo(String nombre){
 
-        if(this.papa.pareja!=null){
+        System.out.println(this.papa.getPareja());
 
-            Animal hijo= new Animal();
-            hijo.nombre=nombre;
-            hijo.peso=1;
+        if(this.papa.getPareja() != null){
+
+            Animal hijo = new Animal();
+            hijo.setNombre(nombre);
+            hijo.setPeso(1);
+
             double random = Math.random();
-            if(random<0.5){
-                hijo.genero="F";
+            if(random < 0.5){
+                hijo.setGenero("F");
             }else{
-                hijo.genero="M";
+                hijo.setGenero("M");
             }
-            this.hijo=hijo;
+            this.hijo = hijo;
 
         }else{
             System.out.println("Aun no puedes tener hijos");
